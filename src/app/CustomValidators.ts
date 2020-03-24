cdac9ee20c1cdb290772b8bce273e7be99030428
kumarkanhaiya <kanhaiyakumarcse@gmail.com>
2020-03-24 14:36:30 +0530

    Custom Asynchronous Validator

--- src/app/CustomValidators.ts
+++ src/app/CustomValidators.ts
@@ -1,9 +1,23 @@
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+
 export class CustomValidators{
     static invalidProjectName(projectName : FormControl) : {[s : string] : boolean} {
         if(projectName.value === 'test'){
             return {'invalidProjectName' : true}
         }
         return null;
+    }
+
+    static asyncInvalidProjectName(projectName : FormControl) : Promise<any> | Observable<any> {
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(projectName.value ==='TestProject'){
+                    resolve({'invalidProjectName' : true});
+                }
+                resolve(null);
+            } , 2500);
+        });
+        return promise;
     }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
     this.projectForm = new FormGroup({
-      'projectName' : new FormControl(null, [Validators.required, CustomValidators.invalidProjectName]),
+      'projectName' : new FormControl(null, [Validators.required, CustomValidators.invalidProjectName], CustomValidators.asyncInvalidProjectName),
       'email' : new FormControl(null, [Validators.required, Validators.email]),
       'projectStatus' : new FormControl('critical')
     });
